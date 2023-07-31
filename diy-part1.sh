2fc29ac0fa13640be5964beac2155501a1206f33
ysxhub888 <139959217+ysxhub888@users.noreply.github.com>
2023-07-31 23:04:23 +0800

    Add files via upload

---
+++ diy-part1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+# Add a feed source
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
